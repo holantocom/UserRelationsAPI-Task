d98a6ddf0f060e0596142199b7b4cd3fcbb357ef
Ilya Yermolovich <ilya1099@mail.ru>
2020-08-28 12:50:21 +0300

    Add migration for table

--- src/Database.php
+++ src/Database.php
@@ -8,9 +8,27 @@ class Database {
     private function __construct($connection)
     {
         $this->_connection = $connection;
+
         $this->_connection->query("SET NAMES 'utf8'");
         $this->_connection->query("SET CHARACTER SET 'utf8'");
         $this->_connection->query("SET SESSION collation_connection = 'utf8_general_ci'");
+
+        $request = $this->loadData("SHOW TABLES LIKE 'user_relations';");
+        if(!$request['count']){
+            $this->_connection->query("CREATE TABLE IF NOT EXISTS `user_relations` (
+              `id` int(11) NOT NULL,
+              `user_id` int(11) NOT NULL,
+              `relation_id` int(11) NOT NULL,
+              `relation_type` enum('friend','foe') NOT NULL,
+              `isDeleted` tinyint(1) NOT NULL DEFAULT '0'
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
+            $this->_connection->query("ALTER TABLE `user_relations`
+              ADD PRIMARY KEY (`id`),
+              ADD KEY `idx_user_id` (`user_id`);");
+            $this->_connection->query("ALTER TABLE `user_relations`
+            MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;");
+        }
+
     }
 
     public static function getInstance()
